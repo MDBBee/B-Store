1791d896fa65f74bfee6377d0499914ff128dd7f
MDBBee <bobbyugbebor@gmail.com>
2025-01-21 14:22:31 +0200

    Loading & not-found page ✅

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,9 @@
-import { Button } from '@/components/ui/button';
-
 export const metadata = {
   title: 'Home',
 };
 
-const Homepage = () => {
-  return (
-    <div>
-      <Button>Submit</Button>
-      B-store Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
-      dolores iste commodi aut dolorum. Ducimus et est dolorem minima adipisci
-      quisquam maxime molestias temporibus in iusto? Laboriosam eum recusandae
-      placeat.
-    </div>
-  );
+const Homepage = async () => {
+  return <div>B-store</div>;
 };
 
 export default Homepage;
