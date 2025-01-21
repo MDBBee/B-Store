70aa428546dac7c097913ba1d0b611beb80c703d
MDBBee <bobbyugbebor@gmail.com>
2025-01-21 12:05:47 +0200

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button';
+
+export const metadata = {
+  title: 'Home',
+};
+
+const Homepage = () => {
+  return (
+    <div>
+      <Button>Submit</Button>
+      B-store Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
+      dolores iste commodi aut dolorum. Ducimus et est dolorem minima adipisci
+      quisquam maxime molestias temporibus in iusto? Laboriosam eum recusandae
+      placeat.
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { Geist_Mono } from 'next/font/google';
+import '@/assets/styles/globals.css';
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+
+const geistmono = Geist_Mono({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | B-store`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistmono.className}`}>{children}</body>
+    </html>
+  );
+}
