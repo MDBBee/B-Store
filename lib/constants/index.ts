7d3f7522cbb888d6a5005447db2c191de4c827fe
MDBBee <bobbyugbebor@gmail.com>
2025-01-23 10:47:15 +0200

    NextAuth & callbackUrl

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,6 +8,6 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: 'bobby',
-  password: 'abcdef',
+  email: '',
+  password: '',
 };
