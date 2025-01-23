45eea7a0072cad6eb4e882d353e9a7cf7a94dbec
MDBBee <bobbyugbebor@gmail.com>
2025-01-23 10:46:23 +0200

    NextAuth & callbackUrl

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,73 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { signInWithCredentials } from '@/lib/actions/user.action';
+// import { signInDefaultValues } from '@/lib/constants';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
+
+const SignInButton = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button className="w-full" variant="default" disabled={pending}>
+      {pending ? 'Signing In...' : 'Sign In'}
+    </Button>
+  );
+};
+
+const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: '',
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+  return (
+    <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
+      <div className="space-y-6">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            required
+            name="email"
+            autoComplete="email"
+            // defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            required
+            name="password"
+            autoComplete="password"
+            // defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <div>
+          <SignInButton />
+        </div>
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+        <div className="text-sm text-center text-muted-foreground">
+          Don&apos;t have an account ?{' '}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+export default CredentialsSignInForm;
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import { handlers } from '@/auth';
+export const { GET, POST } = handlers;
+
+// export const { handlers, signIn, signOut, auth } = NextAuth(config);
+
+// import NextAuth from "next-auth"
+
+// const handler = NextAuth({
+//   ...
+// })
+
+// export { handler as GET, handler as POST }
