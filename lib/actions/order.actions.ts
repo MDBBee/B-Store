5029ec425480ed55bf089e8c4a2594b28ccaa2e7
MDBBee <bobbyugbebor@gmail.com>
2025-02-06 06:58:00 +0200

    Added email receipt functionality

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -6,12 +6,13 @@ import { getMyCart } from './cart.actions';
 import { getUserById } from './user.action';
 import { insertOrderSchema } from '../validators';
 import { prisma } from '@/db/prisma';
-import { CartItem, PaymentResult } from '@/types';
+import { CartItem, PaymentResult, ShippingAddress } from '@/types';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { paypal } from '../paypal';
 import { revalidatePath } from 'next/cache';
 import { PAGE_SIZE } from '../constants';
 import { Prisma } from '@prisma/client';
+import { sendPurchaseReceipt } from '@/email';
 // import { PAGE_SIZE } from '../constants';
 // import { sendPurchaseReceipt } from '@/email';
 
@@ -246,6 +247,14 @@ export async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error('Order not found');
+
+  sendPurchaseReceipt({
+    order: {
+      ...updatedOrder,
+      shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+      paymentResult: updatedOrder.paymentResult as PaymentResult,
+    },
+  });
 }
 
 // Get user's orders
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -58,4 +58,7 @@ export const USER_ROLES = process.env.USER_ROLES
 export const reviewFormDefaultValues = {
   title: '',
   comment: '',
+  rating: 0,
 };
+
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev';
